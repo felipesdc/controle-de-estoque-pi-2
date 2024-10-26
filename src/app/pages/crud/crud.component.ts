e70e83460692ac628ad7328a69e8f443f3a50664
Felipe Santiago de Carvalho <felipesdc@gmail.com>
2024-10-26 14:55:47 -0300

    acerto de subscribe em rotas

--- src/app/pages/crud/crud.component.ts
+++ src/app/pages/crud/crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-crud',
@@ -9,10 +10,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CrudComponent implements OnInit {
   public itemSelecionado = 'produto';
 
-  constructor(private router: Router, private route: ActivatedRoute) {
-    router.events.subscribe((val) => {
-      console.log(val);
-    });
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap
+      .pipe(map((params: ParamMap) => params.get('item')))
+      .subscribe((item) => (this.itemSelecionado = item));
   }
 
   ngOnInit(): void {}
